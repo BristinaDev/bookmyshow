53fa9d7ef94e8eb3b98e8d8747f9f4bfcc8c651a
BristinaDev <devmode26@gmail.com>
2024-08-11 00:33:26 +0530

    Update the from views CSS

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,13 +7,6 @@ class MoviesController < ApplicationController
 
   def show
     @movie = Movie.find(params[:id])
-
-    if params[:show_id]
-      @show = @movie.shows.find(params[:show_id])
-    else
-      @show = @movie.shows.first
-    end
-
     render :show
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,60 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# Seed Movies
+movies = [
+  { title: "Laila Majnu", genre: "LoveStory" },
+  { title: "The Dark Knight", genre: "Action" },
+  { title: "Bahubali", genre: "Historical" },
+  { title: "Cars", genre: "Animations" },
+  { title: "Avengers: Endgame", genre: "Superhero" }
+]
+
+movies.each do |movie_data|
+  Movie.create!(movie_data)
+end
+
+Movie.all.each do |movie|
+  5.times do |i|
+    Show.create!(
+      show_time: DateTime.now + i.days,
+      movie: movie
+    )
+  end
+end 
+
+Show.all.each do |show|
+  (1..5).each do |row|
+    (1..10).each do |number|
+      Seat.create!(
+        seat_row: row,
+        seat_number: number,
+        show: show
+      )
+    end
+  end
+end
+
+users = [
+  { email: "bris@gmail.com", password: "password", name: "Bristina" },
+  { email: "user@gmail.com", password: "password", name: "Ji" }
+]
+
+users.each do |user_data|
+  User.create!(user_data)
+end
+
+users = User.all
+shows = Show.all
+seats = Seat.where(booked: false).sample(5)
+
+Booking.create!(
+  number_of_seats: seats.count.to_s,
+  user: users.first,
+  show: shows.sample,
+  seat_ids: seats.pluck(:id)
+)
+
+seats.each { |seat| seat.update!(booked: true) }
+
+puts "Seeding completed!"
